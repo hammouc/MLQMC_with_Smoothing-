94690dd649d8b5bc08320d7a811f14604b3f2605
Jonas Nießen <jonas.niessen@gmx.de>
2022-05-30 16:24:00 +0200

    update

--- Code/QMC_without_smoothing_call.m
+++ Code/QMC_without_smoothing_call.m
@@ -14,7 +14,7 @@ X_0 = 100;
 %Q = 1/(N-1)^(0.5).*randn(N-1,M);
 P = sobolset(N-1);
 P = scramble(P,'MatousekAffineOwen');
-Q = 1/sqrt(N-1).*transpose(norminv(net(P, M)));
+Q = 1/sqrt(N-1).*transpose(norminv(net(P, 15*M)));
 
 samplepayoffs = zeros(M,1);
 evs = zeros(M,1);
@@ -30,29 +30,42 @@ for samples = 1:M
     evs(samples) = samplepayoffs(samples);
 end
 
-refsol = sum(evs)/M
-
-weakerror = zeros(M,1);
-rmse = zeros(M,1);
-for samples = 1:M
-    rmse(samples) = mean(abs(evs(1:samples)-refsol).^2);
-    weakerror(samples) = abs(mean(evs(1:samples))-refsol);
-end
-
-variance = var(samplepayoffs);
-bound = zeros(shifts*gen_vec,1);
-boundrate = 0.5;
-for j=1:shifts
-    for i = 1:gen_vec
-        bound(i+(j-1)*gen_vec) = 1.96*sqrt(variance)/(((j-1)*gen_vec + i)^boundrate);
+avg = 0;
+for samples = 1:(15*M)
+    X = X_0;
+    for n = 1:N-1
+        X = X + sigma*X*Q(n, samples);
+    end
+    if X >= K
+        avg = avg + X-K;
     end
 end
 
+refsol = avg/(15*M);
 
+weakerror = zeros(M,1);
+variances = zeros(M,1);
+variance = zeros(M,1);
+rmse = zeros(M,1);
+for samples = 1:M
+    variances(samples) = 1/samples*sum(evs(1:samples));
+    variance(samples) = var(variances(1:samples));
+    weakerror(samples) = abs(mean(evs(1:samples))-refsol);
+end
+%% 
 
-loglog(1:M, weakerr, 'blue', 1:M, bound, 'red');
+%variance = var(samplepayoffs);
+bound = zeros(shifts*gen_vec,1);
+boundrate = 0.96;
+for j=1:M
+    bound(j) = 814*1/j^boundrate;
+end
+
+%% 
+
+loglog(1:M, weakerror, 'blue', 1:M, variance, 'red', 1:M, bound);
 title("QMC for the call option and " + M + " samples (without smoothing)", 'Interpreter', 'latex');
 xlabel("M", 'Interpreter', 'latex');
-ylabel("Error (Variance: " + variance + ")", 'Interpreter','latex');
-legend("Exact error", "Error fit of order $M^{-" + boundrate + "}$", 'Interpreter', 'latex');
+ylabel("Error (Variance: " + variance(M) + ")", 'Interpreter','latex');
+legend("Exact error", "Variance of the estimator (with fitting rate $M^{-" + boundrate + "}$)", 'Interpreter', 'latex');
 %saveas(gcf,'../Slides/Figure/QMC_Call_without_Smoothing.svg');
